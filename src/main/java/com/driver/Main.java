494796a41eb006c9db6c93d1aea28ae2d0ab6191
sreetharraj <sreetharradha188@gmail.com>
2024-03-10 12:33:31 +0530

    encapsulation over

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rowonly = new RWOnly("Sreethar");
+
+        //  System.out.print(rowonly.name);
+        //  Name has private access in RWOnly
+
+        rowonly.setName("vijay");
+
+        String nam = rowonly.getName();
+        System.out.println(nam);
+    }
+
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    RWOnly(String name)
+    {
+        this.name=name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+
 }
